83db7d57bc1e144cf47199f61abc05d504c2a80e
Yuchen Xia <stilex@caliber.eecs.umich.edu>
2025-07-11 03:11:37 -0400

    update requirements

--- README.md
+++ README.md
@@ -17,7 +17,9 @@ Then install all required libraries
 
 ```bash
 # For Nvidia GPU install PyTorch 2.1.0 compatible with CUDA 11.8
-pip install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install https://download.pytorch.org/whl/cu118/torch-2.1.0%2Bcu118-cp310-cp310-linux_x86_64.whl
+pip install https://download.pytorch.org/whl/cu118/torchvision-0.16.0%2Bcu118-cp310-cp310-linux_x86_64.whl#sha256=033712f65d45afe806676c4129dfe601ad1321d9e092df62b15847c02d4061dc
+pip install https://download.pytorch.org/whl/cu118/torchaudio-2.1.0%2Bcu118-cp310-cp310-linux_x86_64.whl#sha256=cdfd0a129406155eee595f408cafbb92589652da4090d1d2040f5453d4cae71f
 # For AMD GPU install PyTorch 2.3.0 compatible with ROCM 5.6
 pip install torch==2.3.0.dev20240111+rocm5.6 torchvision==0.18.0.dev20240116+rocm5.6
 ```
--- serving/latent_embedding_generation.py
+++ serving/latent_embedding_generation.py
@@ -6,6 +6,7 @@ import pandas as pd
 from diffusers import (
     StableDiffusion3Pipeline,
     FlowMatchEulerDiscreteScheduler,
+    DiffusionPipeline
 )
 from tqdm import tqdm
 import os
@@ -18,12 +19,13 @@ import argparse
 parser = argparse.ArgumentParser(description="cache directory")
 parser.add_argument("--cache_directory", type=str, required=False, help="directory of cached images")
 args = parser.parse_args()
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
 image_directory = args.cache_directory
 image_paths = [os.path.join(image_directory, img_file) for img_file in os.listdir(image_directory) if img_file.endswith(('.png', '.jpg', '.jpeg'))]
 
-pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
-pipe = pipe.to("cuda")
+# pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
+pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16).to(device)
+# pipe = pipe.to("cuda")
 
 strengths = [ 0.9, 0.8, 0.7, 0.6, 0.5]
 def extract_prompt(filename):
@@ -33,7 +35,7 @@ def extract_prompt(filename):
 
 cached_latents = None
 
-num_chunks = 3
+num_chunks = 4
 chunk_size = len(image_paths) // num_chunks
 
 for j in range(num_chunks):
@@ -47,12 +49,18 @@ for j in range(num_chunks):
         prompt = extract_prompt(path)
         init_image = Image.open(path).convert("RGB")
         for strength in strengths:
+            # latent = pipe.get_latents(
+            #     prompt=prompt,
+            #     image=init_image,
+            #     strength=strength,
+            #     guidance_scale=7.5,
+            #     negative_prompt=None
+            # )
             latent = pipe.get_latents(
                 prompt=prompt,
                 image=init_image,
                 strength=strength,
                 guidance_scale=7.5,
-                negative_prompt=None
             )
             if cached_latents is None:
                 cached_latents = latent.cpu().clone()
@@ -61,6 +69,6 @@ for j in range(num_chunks):
 
     # Print and save
     print(f"Chunk {j}: final cached_latents size: {cached_latents.shape}")
-    save_path = f"cached_latents_{j}.pt"
+    save_path = f"/home/stilex/MoDM/serving/flux_latents/cached_latents_{j}.pt"
     torch.save(cached_latents, save_path)
     print(f"Saved cached latents to {save_path}")
